8b7867eae294928db8a77e785e8c83f8e75cd562
Grace <wanggrace730@gmail.com>
2024-03-23 20:24:36 -0700

    added button

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Button from '@mui/material/Button';
-import NavBar from "./components/navBar";
+import Button from "react-bootstrap/Button";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
-      {/* <StyledMenu
-        id="demo-customized-menu"
-        MenuListProps={{
-          "aria-labelledby": "demo-customized-button",
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose} disableRipple>
-          <EditIcon />
-          Edit
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <FileCopyIcon />
-          Duplicate
-        </MenuItem>
-        <Divider sx={{ my: 0.5 }} />
-        <MenuItem onClick={handleClose} disableRipple>
-          <ArchiveIcon />
-          Archive
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <MoreHorizIcon />
-          More
-        </MenuItem>
-      </StyledMenu> */}
+      <div className="joinButton">
+        <Button variant="outline-dark">Start Your Journey</Button>{" "}
+      </div>
     </div>
   );
 }
