f72b106f8bb6236d21d0c1cc91a0c710d0b562c1
Grace <wanggrace730@gmail.com>
2024-03-23 19:34:23 -0700

    added to navbar

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,4 +1,5 @@
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import "../App.css";
@@ -9,20 +10,14 @@ function NavBar() {
       <Container className="bg-body-tertiary">
       <img src="/fitsphere.png" class="rounded-float-left" alt="..."/>
         <Navbar.Brand className="logo" href="#home">FitSphere</Navbar.Brand>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+        <NavDropdown className="workouts" title="Workouts" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#gymWorkouts">Gym Workouts</NavDropdown.Item>
+              <NavDropdown.Item href="#homeWorkouts">Home Workouts (No Equipment)</NavDropdown.Item>
             </NavDropdown>
+        <Nav.Link href="#progress">Progress</Nav.Link>
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Signed in as: <a href="#login">Theo Bakshi</a>
+            Signed in as: <a href="#login">Grace Wang</a>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
