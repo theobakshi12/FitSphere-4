dd51d8f97ecf11b6bb3f7c913e3bff643c9f3bec
Grace <wanggrace730@gmail.com>
2024-03-23 21:25:08 -0700

    added button

--- client/src/App.js
+++ client/src/App.js
@@ -6,33 +6,9 @@ function App() {
   return (
     <div>
       <NavBar></NavBar>
-      {/* <StyledMenu
-        id="demo-customized-menu"
-        MenuListProps={{
-          "aria-labelledby": "demo-customized-button",
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose} disableRipple>
-          <EditIcon />
-          Edit
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <FileCopyIcon />
-          Duplicate
-        </MenuItem>
-        <Divider sx={{ my: 0.5 }} />
-        <MenuItem onClick={handleClose} disableRipple>
-          <ArchiveIcon />
-          Archive
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <MoreHorizIcon />
-          More
-        </MenuItem>
-      </StyledMenu> */}
+      <div className="joinButton">
+        <Button variant="outline-dark">Start Your Journey</Button>{" "}
+      </div>
     </div>
   );
 }
